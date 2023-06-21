628d22249be2a33608fd927db45b45aff6fc82e2
Refrain <72150044+refrain-net@users.noreply.github.com>
2023-06-21 19:19:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let id = requestAnimationFrame(function hide () {
+requestAnimationFrame(function hide () {
   [...document.querySelectorAll('[data-testid="placementTracking"]')].filter(el => {
     return [...el.querySelectorAll('span')].find(el => el.textContent === 'プロモーション');
   }).map(el => el.style.display = 'none');
