ffa745bba2b1fb9cb8ea93bcdfbe33d488c24249
Refrain <72150044+refrain-net@users.noreply.github.com>
2023-07-05 22:02:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 'use strict';
 
-requestAnimationFrame(function hide () {
-    [...document.querySelectorAll('[data-testid="placementTracking"]')]
-        .filter(element => [...element.querySelectorAll('span')].find(span => span.textContent === 'プロモーション'))
-        .map(element => element.remove());
-    requestAnimationFrame(hide);
+/**
+ * @version 2
+ * @update 2023-07-05 ○○からのプロモーションに対応するため、SVGを検索して削除する
+ */
+let id = requestAnimationFrame(function hide () {
+  [...document.querySelectorAll('[data-testid="placementTracking"]')]
+      .filter(element => element.querySelector('svg'))
+      .map(element => element.remove());
+  id = requestAnimationFrame(hide);
 });
+
+/**
+ * @version 1
+ * @update 2023-06-21 プロモーションと記載されたツイートを削除する
+ /*/
+let id = requestAnimationFrame(function hide () {
+  [...document.querySelectorAll('[data-testid="placementTracking"]')]
+      .filter(element => [...element.querySelectorAll('span')].find(span => span.textContent === 'プロモーション'))
+      .map(element => element.remove());
+  requestAnimationFrame(hide);
+});
+*/
